af413ea9a2c5e9417830f8ac9e1b7cf88089dabe
Aleksey Katargin <gureedo@gmail.com>
2016-09-14 09:47:31 +0500

    v3.0

--- README.md
+++ README.md
@@ -1,27 +1,33 @@
-# rlm_zeromq
+# FreeRADIUS 3.x ZeroMQ module
 
-### Build instructions
-Separate build Using CMAKE:
-```bash
-mkdir build
-cd build
-cmake ..
-make
-```
-
-For in-tree build just copy source files to `src/modules/rlm_zeromq` and add module in Make.inc MODULES section.
+Simple module for performing ZeroMQ PUSH operations.
 
 ### Config examples
 ```
-# example
-zeromq example {
+zeromq zeromq_push_example {
+    # Server address string
+    # Read more about string format here: [http://api.zeromq.org/4-1:zmq-connect]
     server = "tcp://127.0.0.1:5670/"
-    format = "raw"
-    lazy_connect = yes
-    "data" = "{ \
+    
+    # Connection pool
+    pool {
+        start = ${thread[pool].start_servers}
+        min = ${thread[pool].min_spare_servers}
+        max = ${thread[pool].max_servers}
+        spare = ${thread[pool].max_spare_servers}
+        uses = 0
+        lifetime = 0
+        idle_timeout = 60
+    }
+
+    # Data format ("raw" or "bson" is supported)
+    format = bson
+
+    # Data to send
+    data = "{ \
         \"ip\": \"%{Framed-Ip-Address}\", \
         \"nas_ip\": \"%{Nas-Ip-Address}\", \
         \"login\": \"%{User-Name}\" \
-    }" 
+    }"
 }
 ```
--- rlm_zeromq.c
+++ rlm_zeromq.c
@@ -1,194 +1,230 @@
-#ifdef STANDALONE_BUILD
-
-#include <freeradius/radiusd.h>
-#include <freeradius/modules.h>
-
-#else
 #include <freeradius-devel/radiusd.h>
 #include <freeradius-devel/modules.h>
-#endif
-
 #include <zmq.h>
 #include <bson.h>
 
 #define RLM_ZEROMQ_FORMAT_RAW_STR "raw"
 #define RLM_ZEROMQ_FORMAT_BSON_STR "bson"
 
-enum rlm_zeromq_format
-{
-    RLM_ZEROMQ_FORMAT_RAW,
-    RLM_ZEROMQ_FORMAT_BSON
+#define min(x, y) ((x) < (y) ? (x) : (y))
+
+enum rlm_zeromq_format {
+  RLM_ZEROMQ_FORMAT_RAW,
+  RLM_ZEROMQ_FORMAT_BSON
 };
 
-typedef struct rlm_zeromq_t
-{
-    struct
-    {
-        char *server;
-        char *format;
-        char *data;
-        bool lazy_connect;
-    } cfg;
+typedef struct rlm_zeromq_t {
+  struct {
+    const char *server;
+    const char *format;
+    vp_tmpl_t *data;
+  } cfg;
 
-    int format;
-    void *zmq_ctx;
-    void *zmq_sock;
-    bool connected;
+  int format;
+  const char *name;
+  void *zmq_ctx;
+  fr_connection_pool_t *pool;
 } rlm_zeromq_t;
 
-static CONF_PARSER module_config[] = {
-        {"server",       PW_TYPE_STRING_PTR, offsetof(rlm_zeromq_t, cfg.server),       NULL, ""},
-        {"format",       PW_TYPE_STRING_PTR, offsetof(rlm_zeromq_t, cfg.format),       NULL, ""},
-        {"data",         PW_TYPE_STRING_PTR, offsetof(rlm_zeromq_t, cfg.data),         NULL, ""},
-        {"lazy_connect", PW_TYPE_BOOLEAN,    offsetof(rlm_zeromq_t, cfg.lazy_connect), NULL, "yes"},
-        {NULL, -1, 0,                                                                  NULL, NULL}
+typedef struct rlm_zeromq_conn {
+  void *sock;
+} rlm_zeromq_conn_t;
+
+static const CONF_PARSER module_config[] = {
+    {"server", FR_CONF_OFFSET(PW_TYPE_STRING | PW_TYPE_REQUIRED, rlm_zeromq_t, cfg.server), NULL},
+    {"format", FR_CONF_OFFSET(PW_TYPE_STRING | PW_TYPE_REQUIRED, rlm_zeromq_t, cfg.format), NULL},
+    {"data", FR_CONF_OFFSET(PW_TYPE_STRING | PW_TYPE_TMPL | PW_TYPE_REQUIRED, rlm_zeromq_t, cfg.data), NULL},
+    CONF_PARSER_TERMINATOR
 };
 
-static int zeromq_detach(void *instance)
-{
-    rlm_zeromq_t *inst = instance;
+/**
+ * Detach module.
+ * @param[in] instance Module instance.
+ * @return Zero on success.
+ */
+static int mod_detach(void *instance) {
+  rlm_zeromq_t *inst = instance;
 
-    if (inst->zmq_sock) {
-        if (0 != zmq_close(inst->zmq_sock)) {
-            radlog(L_ERR, "rlm_zeromq: Ungraceful zeromq socket close: %s", zmq_strerror(errno));
-        }
+  fr_connection_pool_free(inst->pool);
+
+  if (inst->zmq_ctx) {
+    if (0 != zmq_ctx_destroy(inst->zmq_ctx)) {
+      ERROR("rlm_zeromq (%s): ungraceful zeromq context close: %s", inst->name, zmq_strerror(errno));
     }
-    if (inst->zmq_ctx) {
-        if (0 != zmq_ctx_destroy(inst->zmq_ctx)) {
-            radlog(L_ERR, "rlm_zeromq: Ungraceful zeromq context close: %s", zmq_strerror(errno));
-        }
-    }
+  }
 
-    free(inst);
-
-    return 0;
+  return 0;
 }
 
-static int zeromq_connect(rlm_zeromq_t *inst)
-{
-    if (0 != zmq_connect(inst->zmq_sock, inst->cfg.server)) {
-        radlog(L_ERR, "rlm_zeromq: Failed to connect to '%s': %s", inst->cfg.server, zmq_strerror(errno));
-        return -1;
-    } else {
-        radlog(L_INFO, "rlm_zeromq: Connected to %s", inst->cfg.server);
+/**
+ * Module connection destructor.
+ * @param[in] conn Connection handle.
+ * @return Zero on success.
+ */
+static int mod_conn_free(rlm_zeromq_conn_t *conn) {
+  if (conn->sock) {
+    if (0 != zmq_close(conn->sock)) {
+      ERROR("rlm_zeromq: ungraceful zeromq socket close: %s", zmq_strerror(errno));
     }
+  }
 
-    return 0;
+  DEBUG("rlm_zeromq: closed connection");
+  return 0;
 }
 
-static int zeromq_instantiate(CONF_SECTION *conf, void **instance)
-{
-    rlm_zeromq_t *inst;
+/**
+ * Module connection constructor.
+ * @param[in] ctx Talloc context.
+ * @param[in] instance Module instance.
+ * @return NULL on error, else a connection handle.
+ */
+static void *mod_conn_create(TALLOC_CTX *ctx, void *instance) {
+  rlm_zeromq_t *inst = instance;
 
-    inst = rad_malloc(sizeof(rlm_zeromq_t));
-    if (!inst) {
-        return -1;
-    }
-    memset(inst, 0, sizeof(*inst));
+  void *sock = NULL;
 
-    if (cf_section_parse(conf, inst, module_config) < 0) {
-        goto err;
-    }
+  sock= zmq_socket(inst->zmq_ctx, ZMQ_PUSH);
+  if (!sock) {
+    ERROR("rlm_zeromq (%s): failed to create socket: %s", inst->name, zmq_strerror(errno));
+    goto err;
+  }
 
-    if (0 == strcasecmp(inst->cfg.format, RLM_ZEROMQ_FORMAT_RAW_STR)) {
-        inst->format = RLM_ZEROMQ_FORMAT_RAW;
-    } else if (0 == strcasecmp(inst->cfg.format, RLM_ZEROMQ_FORMAT_BSON_STR)) {
-        inst->format = RLM_ZEROMQ_FORMAT_BSON;
-    } else {
-        radlog(L_ERR, "rlm_zeromq: Invalid output format data type '%s', only 'raw' or 'bson' is acceptable",
-               inst->cfg.format);
-        goto err;
-    }
+  if (zmq_connect(sock, inst->cfg.server) != 0) {
+    ERROR("rlm_zeromq (%s): failed to connect to '%s': %s", inst->name, inst->cfg.server, zmq_strerror(errno));
+    goto err;
+  }
 
-    inst->zmq_ctx = zmq_ctx_new();
-    if (!inst->zmq_ctx) {
-        radlog(L_ERR, "rlm_zeromq: Failed to create zeromq context: %s", zmq_strerror(errno));
-        goto err;
-    }
+  rlm_zeromq_conn_t *conn = talloc_zero(ctx, rlm_zeromq_conn_t);
+  conn->sock = sock;
+  talloc_set_destructor(conn, mod_conn_free);
 
-    inst->zmq_sock = zmq_socket(inst->zmq_ctx, ZMQ_PUSH);
-    if (!inst->zmq_sock) {
-        radlog(L_ERR, "rlm_zeromq: Failed to create zeromq socket: %s", zmq_strerror(errno));
-        return -1;
-    }
+  return conn;
 
-    if (!inst->cfg.lazy_connect) {
-        if (0 != zeromq_connect(inst)) {
-            goto err;
-        }
-        inst->connected = true;
-    }
-
-    *instance = inst;
-    return 0;
-
-    err:
-    zeromq_detach(inst);
-    return -1;
+  err:
+  if (sock) zmq_close(sock);
+  return NULL;
 }
 
-static int zeromq_proc(void *instance, REQUEST *request)
-{
-    rlm_zeromq_t *inst = instance;
+/**
+ * Instantiate module.
+ * @param[in] conf Module config.
+ * @param[in] instance Module instance.
+ * @return Zero on success.
+ */
+static int mod_instantiate(CONF_SECTION *conf, void *instance) {
+  rlm_zeromq_t *inst = instance;
+  (void) conf;
 
-    if (!inst->connected) {
-        if (0 != zeromq_connect(inst)) {
-            return RLM_MODULE_NOOP;
-        }
-        inst->connected = true;
+  inst->name = cf_section_name2(conf);
+  if (!inst->name) {
+    inst->name = cf_section_name1(conf);
+  }
+
+  if (!strcasecmp(inst->cfg.format, RLM_ZEROMQ_FORMAT_RAW_STR)) {
+    inst->format = RLM_ZEROMQ_FORMAT_RAW;
+  } else if (!strcasecmp(inst->cfg.format, RLM_ZEROMQ_FORMAT_BSON_STR)) {
+    inst->format = RLM_ZEROMQ_FORMAT_BSON;
+  } else {
+    cf_log_err_cs(conf, "invalid 'format' option, use 'raw' or 'bson'");
+    goto err;
+  }
+
+  inst->zmq_ctx = zmq_ctx_new();
+  if (!inst->zmq_ctx) {
+    ERROR("rlm_zeromq (%s): failed to create zeromq context: %s", inst->name, zmq_strerror(errno));
+    goto err;
+  }
+
+  inst->pool = fr_connection_pool_module_init(conf, inst, mod_conn_create, NULL, inst->name);
+  if (!inst->pool) {
+    goto err;
+  }
+
+  return 0;
+
+  err:
+  mod_detach(inst);
+  return -1;
+}
+
+/**
+ * Main module procedure.
+ * @param[in] instance Module instance.
+ * @param[in] request Radius request.
+ * @return One of #rlm_rcode_t.
+ */
+static rlm_rcode_t mod_proc(void *instance, REQUEST *request) {
+  rlm_zeromq_t *inst = instance;
+  rlm_zeromq_conn_t *conn = NULL;
+  rlm_rcode_t code = RLM_MODULE_FAIL;
+
+  char *data = NULL;
+  bson_t *bson_data = NULL;
+  const void *msg = NULL;
+  size_t msg_len = 0;
+
+  conn = fr_connection_get(inst->pool);
+  if (!conn) {
+    goto end;
+  }
+
+  ssize_t data_len = tmpl_aexpand(request, &data, request, inst->cfg.data, NULL, NULL);
+  if (data_len < 0) {
+    RERROR("failed to substitute attributes for data '%s'", inst->cfg.data->name);
+    goto end;
+  }
+
+  if (inst->format == RLM_ZEROMQ_FORMAT_BSON) {
+    bson_error_t error;
+
+    bson_data = bson_new_from_json((uint8_t *) data, data_len, &error);
+    if (!bson_data) {
+      RERROR("JSON->BSON conversion failed for '%s': %d.%d %s", data, error.domain, error.code, error.message);
+      goto end;
     }
 
-    const void *data = NULL;
-    size_t data_len = 0;
+    msg = bson_get_data(bson_data);
+    msg_len = bson_data->len;
+  } else {
+    msg = data;
+    msg_len = (size_t) data_len;
+  }
 
-    bson_t *doc = NULL;
+  if (TEMP_FAILURE_RETRY(zmq_send(conn->sock, msg, msg_len, 0)) == -1) {
+    RERROR("failed to send message: %s", zmq_strerror(errno));
+  } else {
+    code = RLM_MODULE_OK;
+  }
 
-    char pre_data[8192];
-    int pre_data_len = radius_xlat(pre_data, sizeof(pre_data), inst->cfg.data, request, NULL);
-
-    if (RLM_ZEROMQ_FORMAT_BSON == inst->format) {
-        bson_error_t error;
-
-        doc = bson_new_from_json((uint8_t *) pre_data, pre_data_len, &error);
-        if (!doc) {
-            radlog(L_ERR, "rlm_zeromq: JSON->BSON conversion failed for '%s': %d.%d %s",
-                   pre_data, error.domain, error.code, error.message);
-            return RLM_MODULE_NOOP;
-        }
-
-        data = bson_get_data(doc);
-        data_len = doc->len;
-    } else {
-        data = pre_data;
-        data_len = (size_t) pre_data_len;
-    }
-
-    if (-1 == zmq_send(inst->zmq_sock, data, data_len, 0)) {
-        radlog(L_ERR, "rlm_zeromq: Failed to send message: %s", zmq_strerror(errno));
-    }
-
-    if (doc) {
-        bson_destroy(doc);
-    }
-
-    return RLM_MODULE_NOOP;
+  end:
+  if (conn) fr_connection_release(inst->pool, conn);
+  if (bson_data) bson_destroy(bson_data);
+  return code;
 }
 
 /* globally exported name */
+extern module_t rlm_zeromq;
 module_t rlm_zeromq = {
-        RLM_MODULE_INIT,
-        "zeromq",
-        RLM_TYPE_THREAD_UNSAFE,
-        zeromq_instantiate,   /* instantiation */
-        zeromq_detach,        /* detach */
-        {
-                zeromq_proc,  /* authentication */
-                zeromq_proc,  /* authorization */
-                zeromq_proc,  /* preaccounting */
-                zeromq_proc,  /* accounting */
-                NULL,         /* checksimul */
-                zeromq_proc,  /* pre-proxy */
-                zeromq_proc,  /* post-proxy */
-                zeromq_proc   /* post-auth */
-        },
+    .magic = RLM_MODULE_INIT,
+    .name = "zeromq",
+    .type = RLM_TYPE_THREAD_SAFE | RLM_TYPE_HUP_SAFE,
+    .inst_size = sizeof(rlm_zeromq_t),
+    .config = module_config,
+    .bootstrap = NULL,
+    .instantiate = mod_instantiate,
+    .detach = mod_detach,
+    .methods = {
+        [MOD_AUTHENTICATE] = mod_proc,
+        [MOD_AUTHORIZE] = mod_proc,
+        [MOD_PREACCT] = mod_proc,
+        [MOD_ACCOUNTING] = mod_proc,
+        [MOD_SESSION] = NULL,
+        [MOD_PRE_PROXY] = mod_proc,
+        [MOD_POST_PROXY] = mod_proc,
+        [MOD_POST_AUTH] = mod_proc,
+#ifdef WITH_COA
+        [MOD_RECV_COA] = mod_proc,
+        [MOD_SEND_COA] = mod_proc,
+#endif
+    },
 };
